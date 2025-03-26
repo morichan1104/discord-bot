2ce24b195a021d8495e3095640f2c6d6817dbe7e
morichan1104 <linkooka@gmail.com>
2025-03-26 13:23:01 +0900

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1,12 +1,13 @@
 from threading import Thread
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
+@app.head("/")
 async def root():
-    return {"message": "Server is Online."}
+    return Response(content='Server is Online.', media_type='text/plain')
 
 def start():
     uvicorn.run(app, host="0.0.0.0", port=8080)
